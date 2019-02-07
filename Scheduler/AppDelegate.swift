2719a09ae381f8d966b3e27a10c6519230ae3fd5
michbraz <michbraz@umich.edu>
2019-02-07 01:38:29 -0500

    inital commit'

---
+++ Scheduler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scheduler
+//
+//  Created by Michael Brazeau on 2/7/19.
+//  Copyright © 2019 Michael Brazeau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
